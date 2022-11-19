d70ed9f805587282b9341e99a512327a84d9ab9b
Madeline Petersen <9222076+maddleline@users.noreply.github.com>
2022-11-19 12:43:26 -0800

    Update timeSince function

--- src/utils/timeSince.js
+++ src/utils/timeSince.js
@@ -1,13 +1,15 @@
 /*
- * timeSince function from Stack Overflow:
+ * timeSince function adapted from Stack Overflow:
  * https://stackoverflow.com/a/3177838/8545523
  * takes in a timestamp and returns a string describing
- * the time since that timestamp in the most appropriate
- * format
+ * the time since that timestamp using the most appropriate
+ * time interval
  */
 export const timeSince = (timestamp) => {
-  const seconds = Math.floor((new Date() - timestamp) / 1000)
-  let interval = seconds / 31536000
+  const date = new Date(timestamp * 1000)
+  var seconds = Math.floor((new Date() - date) / 1000)
+
+  var interval = seconds / 31536000
 
   if (interval > 1) {
     return Math.floor(interval) + ' years'
