5a5fe996a2504a384922e8903d7dbf7a87dbcc8e
Madeline Petersen <9222076+maddleline@users.noreply.github.com>
2022-11-19 23:56:19 -0800

    Add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from 'react-router-dom'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h2>Hacker News</h2>
+      <nav>
+        <NavLink to='/'>latest</NavLink>
+        {` | `}
+        <NavLink to='/saved'>saved</NavLink>
+      </nav>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Layout = () => {
   return (
@@ -7,6 +8,8 @@ const Layout = () => {
       <Header />
 
       <Outlet />
+
+      <Footer />
     </div>
   )
 }
